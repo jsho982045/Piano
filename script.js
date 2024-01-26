0f0bb4e46a4cbf1e6c078ad3f2bf2ce140cfdcbc
Jack E. Shomer <74559111+jsho982045@users.noreply.github.com>
2024-01-26 03:08:14 +0000

    Added 24 more keys, now have 36 total

--- script.js
+++ script.js
@@ -26,7 +26,9 @@ function setup() {
     }
 
     let keyMap = {
-        'C': 261.63, 'C#': 277.18, 'D': 293.66, 'D#': 311.13, 'E': 329.63, 'F': 349.23, 'F#': 369.99, 'G': 392.00, 'G#': 415.30, 'A': 440.00, 'A#': 466.16, 'B': 493.88,
+        'C' : 130.81, 'C#': 138.59, 'D': 146.83, 'D#': 155.56, 'E': 164.81, 'F': 174.61, 'F#': 185.00, 'G': 196.00, 'G#': 207.65, 'A': 220.00, 'A#': 233.08, 'B': 246.94,
+        'C2': 261.63, 'C#2': 277.18, 'D2': 293.66, 'D#2': 311.13, 'E2': 329.63, 'F2': 349.23, 'F#2': 369.99, 'G2': 392.00, 'G#2': 415.30, 'A2': 440.00, 'A#2': 466.16, 'B2': 493.88,
+        'C3': 523.25, 'C#3': 554.37, 'D3': 587.33, 'D#3': 622.25, 'E3': 659.25, 'F3': 698.46, 'F#3': 739.99, 'G3': 783.99, 'G#3': 830.61, 'A3': 880.00, 'A#3': 932.33, 'B3': 987.77,
     };
 
     document.querySelectorAll('.key').forEach(function(key) {
@@ -44,11 +46,23 @@ function setup() {
     });
 
     // Adjust black key positions
-    let blackKeys = document.querySelectorAll('.key.black');
-    let blackKeyOffsets = [40, 80, 170, 215, 265]; // Adjust these values as needed
-    blackKeys.forEach(function(key, index) {
-        key.style.marginLeft = blackKeyOffsets[index] + 'px';
-    });
-}
+    function adjustBlackKeys() {
+        // Define the left offsets for each black key manually
+        let blackKeyOffsets = {
+            'C#': 35, 'D#': 80,
+            'F#': 170, 'G#': 215, 'A#': 260,
+            'C#2': 355, 'D#2': 400,
+            'F#2': 490, 'G#2': 535, 'A#2': 580,
+            'C#3': 675, 'D#3': 720,
+            'F#3': 805, 'G#3': 850, 'A#3': 900
+        };
 
+        document.querySelectorAll('.key.black').forEach((key) => {
+            // Use the offsets defined above to set the position of each black key
+            key.style.left = blackKeyOffsets[key.id] + 'px';
+        });
+    }
+
+    adjustBlackKeys(); // Call the function immediately to adjust the black keys
+}
 window.onload = setup;
